ddb874164488111825a0b2ff75db37f3060daa00
Son0604 <ntqson0604@gmail.com>
2021-08-27 11:48:24 -0700

    update

--- Complete Responsive Event Organizer Website Design Using HTML - CSS - Javascript/script.js
+++ Complete Responsive Event Organizer Website Design Using HTML - CSS - Javascript/script.js
@@ -28,4 +28,26 @@ var swiper = new Swiper(".home-slider", {
         delay: 3000,
         disableOnInteraction: false,
     }
-  });
+});
+
+var swiper = new Swiper(".review-slider", {
+    slidesPerView: 1,
+    grabCursor: true,
+    loop:true,
+    spaceBetween: 10,
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        700: {
+            slidesPerView: 2,
+        },
+        1050: {
+            slidesPerView: 3,
+        },
+    },
+    autoplay:{
+        delay: 5000,
+        disableOnInteraction: false,
+    }
+});
